ededb49c79744bf04eaa8fd45eef7b17c78e4876
Sepoz <simone.boccolari@protonmail.com>
2025-01-12 23:26:10 +0100

    add examples

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,100 @@
-<script setup></script>
+<script setup>
+import { Motion } from 'motion-v'
+
+const container = {
+  hidden: { opacity: 1, scale: 0 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+  },
+}
+
+const items = {
+  hidden: { y: 20, opacity: 0, scale: 0.85 },
+  visible: {
+    y: 0,
+    opacity: 1,
+  },
+}
+
+const list = [0, 1, 2, 3]
+</script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-  </header>
+  <main class="container">
+    <Motion
+      class="motion-primary"
+      :initial="{ scale: 0 }"
+      :animate="{ rotate: 180, scale: 1 }"
+      :transition="{
+        type: 'spring',
+        stiffness: 260,
+        damping: 20,
+        delay: 0.3,
+      }"
+    />
 
-  <main></main>
+    <Motion
+      class="motion-primary"
+      :animate="{
+        scale: [1, 0.5, 0.5, 1, 1],
+        rotate: [0, 0, 180, 180, 0],
+        borderRadius: ['0%', '0%', '20%', '20%', '0%'],
+      }"
+      :transition="{
+        duration: 2,
+        ease: 'easeInOut',
+        times: [0, 0.2, 0.5, 0.8, 1],
+        repeat: Infinity,
+      }"
+    />
+
+    <Motion
+      as="ul"
+      :variants="container"
+      initial="hidden"
+      animate="visible"
+      :transition="{
+        type: 'spring',
+        delayChildren: 0.5,
+        staggerChildren: 0.2,
+      }"
+      class="motion-container"
+    >
+      <Motion v-for="item in list" :key="item" :variants="items" class="motion-primary" />
+    </Motion>
+  </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.container {
+  width: 100vw;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  row-gap: 100px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.motion-primary {
+  background-color: burlywood;
+  width: 33%;
+  aspect-ratio: 1/1;
+  border-radius: 10px;
+  transform-origin: center;
+}
+
+.motion-container {
+  border-radius: 1rem;
+  overflow: hidden;
+  list-style: none;
+  padding: 0.5rem;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+  aspect-ratio: 1 / 1;
+  background-color: rgba(222, 184, 135, 0.2);
+  width: 33%;
 }
 </style>
